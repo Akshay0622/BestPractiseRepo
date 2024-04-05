f485985c161d51115e5e941a6752cd0ddf96d067
Akshay0622 <akshaytanpure226@gmail.com>
2024-04-06 00:23:17 +0530

    positive negative number

---
+++ dotnet/100Codes/NumberPN.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100Codes
+{
+    internal class NumberPN
+    {
+        static void Main(string[] args)
+        {
+
+            int number = 10;
+
+            if (number == 0)
+            {
+                Console.WriteLine("number is zero");
+            }
+            else if (number >0) {
+                Console.WriteLine("number is positive");
+           }
+            else
+            {
+                Console.WriteLine("number is negative");
+            }
+        }
+    }
+}
