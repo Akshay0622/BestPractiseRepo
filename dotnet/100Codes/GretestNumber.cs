fd22c98fa5d9319d197d0dd67ccf8b01ce11b40d
Akshay0622 <akshaytanpure226@gmail.com>
2024-04-07 00:01:53 +0530

    best practise code added

---
+++ dotnet/100Codes/GretestNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100Codes
+{
+    internal class GretestNumber
+    {
+       public static void Main(string[] args)
+        {
+            int a = 50;
+            int b = 20;
+
+            if(a > b) {
+                Console.WriteLine("a is greater than b");
+
+            }
+            else if (a < b) {
+                Console.WriteLine("b is greater than a");
+            }
+            else {
+                Console.WriteLine("a and b are same value");
+            }
+        }
+    }
+}
---
+++ dotnet/100Codes/Sum.cs
@@ -0,0 +1,17 @@
+  namespace _100Codes
+{
+    internal class Sum
+    {
+
+  static void Main(string[] args)
+        {
+        int n = 10;
+
+        int sum = 0;
+
+     for (int i = 1; i <= n; i++)
+          sum += i;
+      Console.WriteLine(sum);
+   }
+ }
+}
